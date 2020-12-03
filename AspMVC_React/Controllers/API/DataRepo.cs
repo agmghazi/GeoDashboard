cd0ff4b23337630e8244b3ea04e57990f85a2342
alieladly2012 <alieladly2012@users.noreply.github.com>
2020-12-03 09:51:30 +0300

    api , models added

--- AspMVC_React/Controllers/API/DataRepo.cs
+++ AspMVC_React/Controllers/API/DataRepo.cs
@@ -1,19 +1,37 @@
-﻿using System;
+﻿using DataModels;
+using DataModels.AwModels;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 
 namespace AspMVC_React.Controllers.API 
 {
     public class DataRepoController : ApiController
     {
         // GET api/<controller>
- 
-        public IEnumerable<string> Get()
+        private PrioritiesDBEntities db = new PrioritiesDBEntities();
+
+        public IHttpActionResult Get(int locationId, string locationType)
         {
-            return new string[] { "pop1", "pop2", "pop3", "pop4", "pop5", "pop6", "pop7", "pop8" };
+          
+            var result = GetDataSetExternalConnection ("Data Source=10.80.60.193;Initial Catalog=PrioritiesDB_Barrom;User ID=awlwiatapp;Password=app1234;", "GetCityPopSumByReg " , "GetCityPopSumByReg", new SqlParameter("@reg", locationId));
+
+            List <KeyValModel> lKyVl = new List<KeyValModel>();
+            foreach (DataColumn item in result.Tables["GetCityPopSumByReg"].Columns)
+            {
+                lKyVl.Add(new KeyValModel { key = item.ColumnName, value = result.Tables["GetCityPopSumByReg"].Rows[0][item.ColumnName].ToString() });
+            }
+
+            var jResult = new JavaScriptSerializer().Serialize(lKyVl);
+
+            return Ok( jResult);
         }
 
         // GET api/<controller>/5
@@ -23,8 +41,14 @@ namespace AspMVC_React.Controllers.API
         }
 
         // POST api/<controller>
-        public void Post([FromBody] string value)
+        public IHttpActionResult Post([FromBody] string value)
         {
+
+            List<string> values = new List<string>();
+
+
+            return Ok("done");
+
         }
 
         // PUT api/<controller>/5
@@ -36,5 +60,32 @@ namespace AspMVC_React.Controllers.API
         public void Delete(int id)
         {
         }
+
+
+
+
+
+
+
+        public static DataSet GetDataSetExternalConnection(string connectionstring, string stored_name, string table_name, params SqlParameter[] prmarr)
+        {
+            SqlConnection con = new SqlConnection(connectionstring);
+            SqlCommand cmd = new SqlCommand(stored_name, con);
+            foreach (SqlParameter prm in prmarr)
+            {
+                cmd.Parameters.Add(prm);
+            }
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds, table_name);
+            return ds;
+        }
+
+
+
+
+
+
     }
 }
---
+++ DataModels/KeyValModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+   public  class KeyValModel
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+    }
+}
